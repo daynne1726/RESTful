0ae8ac8ecaf6865da07e3039c9850af512dacc97
caballeroda_sd2081@CEACCS.NET <caballeroda_sd2081@CEACCS.NET>
2019-10-17 09:57:55 +0800

    ambot ani

---
+++ index.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const app = express();
+const Joi = require('joi');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//database sample
+const users = [
+    {
+        name: 'Keenan',
+        id: 1
+    },
+    {
+        name: 'Paul',
+        id: 2
+    },
+    {
+        name: 'Mendiola',
+        id: 3
+    }
+];
+
+app.get('/', (request, response) => {
+    response.send(users);
+});
+
+
+app.get('/users/:id', (request, response) => {
+    const user = users.find(user => user.id === parseInt(request.params.id));
+    //query database
+    if(!user) response.status(404).send('Course does not exist');
+    response.send(user);
+});
+
+app.post('/users', (request, response) => {
+    
+    const result = validateData(request.body);
+    if(result.error){
+        response.status(404).send(result.error.details[0].message);
+        return;
+    }
+
+    const user = {
+        name: request.body.name,
+        id: users.length + 1
+    }
+
+    //push to database
+    users.push(user);
+    response.send(user);
+});
+
+validateData = (user) => {
+    const schema = {
+        name: Joi.string().min(3).required()
+    }
+    return Joi.validate(user.body, schema);
+}
+
+app.put('/users/:id', (request, response) => {
+    const user = users.find(users => user.id === parseInt(request.params.id));
+    if(!user) response.status(404).send('User does not exist');
+    const {error} = validateData(request.body);
+    if(error){
+        response.send(error);
+        return;
+    }
+
+    user.name = request.body.name;
+    response.send(user);
+});
+
+app.delete('/users/:id', (request, response) => {
+    console.log('delete');
+    const user = users.find(user => user.id === parseInt(request.params.id));
+    if(!user) response.status(404).send('User does not exist');
+
+    const index = users.indexOf(user);
+    user.splice(index, 1);
+
+    response.send('Deleted');
+});
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+});
